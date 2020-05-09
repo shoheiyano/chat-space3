3bfddba9b17e0513af637b6234f0a8aaeb4790fc
shoheiyano <yspersona@yahoo.co.jp>
2020-05-09 12:51:24 +0900

    テスト前まで全部

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "messages#index"
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: %i(edit update)
+  resources :groups, only: %i(index new create edit update) do
+    resources :messages, only: %i(index create)
+  end
 end
