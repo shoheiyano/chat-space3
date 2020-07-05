b7c490914a04c82bc9ec7d8e16aec690b3b89c51
shoheiyano <yspersona@yahoo.co.jp>
2020-07-05 11:19:40 +0900

    一旦全てマージ
    softを試す

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: true }
 
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages
+  
 end
